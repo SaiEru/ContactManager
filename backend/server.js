83b718c3868f54da5ea6c105da37a8fa02083ad1
Erukulla Sai <erukullasai0@gmail.com>
2026-01-02 18:33:54 +0530

    Use Render backend URL for all API calls

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,23 @@ const cors = require("cors");
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/contactdb")
-  .then(() => console.log("MongoDB Connected"));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () =>
+  console.log(`Server running on ${PORT}`)
+);
